d173e2a09b3e58035265740674cc93d13ac28aef
Nagavaibhav <nnagavaibhav@gmail.com>
2025-08-07 18:03:06 +0530

    simple code

--- file1.py
+++ file1.py
@@ -1,4 +1,12 @@
 print("Hello, world!")
 income= float(input("Enter your current income"))
 spendings= float(input("Enter your average spendings"))
-savings= (income-spendings)/ income
+savings= 100*(income-spendings)/ income
+stability= int(input("Enter the number corresponding to your job"))
+cgpa= float
+if( stability < 500):
+    print("Candidate is NOT eligible for loan.")
+
+else:
+    print("Candidate is elligible for loan.")
+print("Savings rate: ",savings," %")
