8c875905e6e21cf38cf518f04c908690bf92b5ba
Usirnim <vedesh2006@gmail.com>
2025-08-07 21:11:43 +0530

    Database updated

---
+++ Backend/backend.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS credit_scoring_db;
+USE credit_scoring_db;
+CREATE TABLE user_financial_profile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cash_inflow DECIMAL(12,2),
+    avg_bank_balance DECIMAL(12,2),
+    location_type ENUM('Urban','Rural','Semi-Urban'),
+    income_type ENUM('Salaried','Gig','Informal'),
+    age_to_employment_ratio DECIMAL(4,2),
+    housing_type ENUM('Owned','Rented','PG'),
+    rent_amount DECIMAL(10,2) NULL,
+    num_occupants INT NULL,
+    bill_payment_consistency DECIMAL(3,2) NULL,
+    bnpl_used BOOLEAN,
+    bnpl_repayment_ratio DECIMAL(3,2) NULL,
+    education_level ENUM('12th','Diploma','Grad','PostGrad'),
+    grade_or_cgpa DECIMAL(4,2) NULL,
+    score DECIMAL(5,2) NULL,
+    decision ENUM('Approved','Rejected') NULL,
+    explanation TEXT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+SELECT * FROM user_financial_profile;
